f7f23bae5fd70feb0d6a31eff0e000b1332dfe91
SuciuPatrick <patrick.suciu97@gmail.com>
2020-11-16 19:41:18 +0200

    added sequence validation

--- lab_4_FA/main.py
+++ lab_4_FA/main.py
@@ -4,6 +4,17 @@ the_alphabet = []
 initial_state = ''
 set_of_final_states = []
 
+
+class Transition:
+    def __init__(self, initial, to, end):
+        self.initial = initial
+        self.to = to
+        self.end = end
+
+    def __str__(self):
+        return f"{self.initial} -> {self.to} -> {self.end}\n"
+
+
 with open('FA.in') as f:
     for count, line in enumerate(f):
         if count == 0:  # read states
@@ -13,9 +24,34 @@ with open('FA.in') as f:
         elif count == 2:  # read initial state
             initial_state = line
         elif count == 3:  # read final state
-            elems = line.split()
+            set_of_final_states = line.split()
         else:  # add transitions
-            transitions.append(line.split())
+            values = line.split()
+            transitions.append(Transition(values[0], values[1], values[2]))
+
+
+def valid_sequence(sequence):
+    actual_state = initial_state.strip()
+
+    for character in sequence:
+        print(actual_state, item)
+        ok = False
+        for transition in transitions:
+            if transition.to == character and actual_state == transition.initial:
+                ok = True
+                print(transition)
+                actual_state = transition.end
+                import pdb;
+                pdb.set_trace()
+                break
+
+        if not ok:
+            return False
+
+    if actual_state in set_of_final_states:
+        return True
+    return False
+
 
 while True:
     print("1. Print states: \n")
@@ -23,19 +59,26 @@ while True:
     print("3. Print the alphabet: \n")
     print("4. Print initial state: \n")
     print("5. Set of final states: \n")
-    print("6. Exit!.")
+    print("6. Check sequence.")
 
     item = str(input('Choose an element from the menu: '))
     if item == '1':
         print(str(states))
     elif item == '2':
-        print(str(transitions))
+        for transition in transitions:
+            print(transition)
     elif item == '3':
         print(str(the_alphabet))
     elif item == '4':
         print(str(initial_state))
     elif item == '5':
         print(str(set_of_final_states))
+    elif item == '6':
+        sequence = str(input("Example: r t c\nSequence: "))
+        if valid_sequence(sequence.split()):
+            print("Valid!")
+        else:
+            print("Invalid!")
     else:
         break
 
